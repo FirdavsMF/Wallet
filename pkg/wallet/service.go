82ff39cd46d7ef7eb094773d66fd8096eb08e95e
Firdavs <firdavs.2001.fed@gmail.com>
2021-04-25 22:22:15 +0500

    ...

--- pkg/wallet/service.go
+++ pkg/wallet/service.go
@@ -15,96 +15,104 @@ import (
 	"github.com/google/uuid"
 )
 
-//ErrPhoneRegistered - телефон уже регитрирован
+// распространённые возможные ошибки
 var ErrPhoneRegistered = errors.New("phone already registred")
-
-//ErrAmountMustBePositive - счёт не может быть пустым
 var ErrAmountMustBePositive = errors.New("amount must be greater than zero")
-
-//ErrAccountNotFound - пользователь не найден
 var ErrAccountNotFound = errors.New("account not found")
-
-//ErrNotEnoughtBalance - на счете недостаточно средств
 var ErrNotEnoughtBalance = errors.New("account not enough balance")
-
-//ErrPaymentNotFound - платеж не найден
 var ErrPaymentNotFound = errors.New("payment not found")
-
-// ErrFavoriteNotFound - Избранное не найдено
 var ErrFavoriteNotFound = errors.New("favorite not found")
-
-// ErrFileNotFound - файл не найден
-var ErrFileNotFound = errors.New("file not fount")
-
+var ErrFileNotFound = errors.New("file not found")
 var err error
 
-// Service представляет информацию о пользователе.
-type Service struct {
-	nextAccountID int64
-	accounts      []*types.Account
-	payments      []*types.Payment
-	favorites     []*types.Favorite
+
+type Service struct  {
+	NextAccountID int64
+	accounts []*types.Account 
+	payments []*types.Payment
+	favorites []*types.Favorite 
 }
 
-// RegisterAccount - метод для регистрация нового прользователя.
+
+// RegisterAccount регистрирует  нового пользователя в системе
 func (s *Service) RegisterAccount(phone types.Phone) (*types.Account, error) {
 	for _, account := range s.accounts {
 		if account.Phone == phone {
 			return nil, ErrPhoneRegistered
-		}
+		}	
 	}
-	s.nextAccountID++
-	account := &types.Account{
-		ID:      s.nextAccountID,
-		Phone:   phone,
+	s.NextAccountID++
+	account := &types.Account {
+		ID : s.NextAccountID,
+		Phone: phone,
 		Balance: 0,
 	}
 	s.accounts = append(s.accounts, account)
+
 	return account, nil
 }
 
-//Pay метод для регистрации платижа
+
+// Deposit пополняет счёт пользователя
+func (s *Service) Deposit(accountID int64, amount types.Money) error {
+	if amount <= 0 {
+		return ErrAmountMustBePositive
+	}
+
+	var account *types.Account
+	for _, acc := range s.accounts {
+		if acc.ID  == accountID {
+			account = acc
+			break
+		}
+	}
+
+	if account == nil {
+		return ErrAccountNotFound
+	}
+
+	account.Balance += amount
+	return nil
+}
+
+
+// Pay платит определенную сумму денег за категорию
 func (s *Service) Pay(accountID int64, amount types.Money, category types.PaymentCategory) (*types.Payment, error) {
 	if amount <= 0 {
 		return nil, ErrAmountMustBePositive
 	}
 
 	var account *types.Account
-
 	for _, acc := range s.accounts {
-		if acc.ID == accountID {
+		if acc.ID  == accountID {
 			account = acc
 			break
 		}
 	}
-
 	if account == nil {
 		return nil, ErrAccountNotFound
 	}
-
 	if account.Balance < amount {
 		return nil, ErrNotEnoughtBalance
 	}
 
 	account.Balance -= amount
-
 	paymentID := uuid.New().String()
-	payment := &types.Payment{
-		ID:        paymentID,
+	payment := &types.Payment {
+		ID: paymentID,
 		AccountID: accountID,
-		Amount:    amount,
-		Category:  category,
-		Status:    types.PaymentStatusInProgress,
+		Amount: amount,
+		Category: category,
+		Status: types.PaymentStatusInProgress,
 	}
-
 	s.payments = append(s.payments, payment)
 	return payment, nil
 }
 
-// FindAccountByID ищем пользователя по ID
+
+// FindAccountById ищет пользователя по ID
 func (s *Service) FindAccountByID(accountID int64) (*types.Account, error) {
 	var account *types.Account
-
 	for _, acc := range s.accounts {
 		if acc.ID == accountID {
 			account = acc
@@ -119,7 +127,8 @@ func (s *Service) FindAccountByID(accountID int64) (*types.Account, error) {
 	return account, nil
 }
 
-// FindPaymentByID ищем платёж по ID
+
+// FindPaymentByID ищет платёж по ID
 func (s *Service) FindPaymentByID(paymentID string) (*types.Payment, error) {
 	var payment *types.Payment
 
@@ -136,7 +145,42 @@ func (s *Service) FindPaymentByID(paymentID string) (*types.Payment, error) {
 	return payment, nil
 }
 
-// FindFavoriteByID ищем платёж по ID в Избранное
+
+// Reject отменяет платёж
+func (s *Service) Reject(paymentID string) error {
+	pay, err := s.FindPaymentByID(paymentID)
+	if err != nil {
+		return ErrPaymentNotFound
+	}
+
+	acc, err := s.FindAccountByID(pay.AccountID)
+	if err != nil {
+		return ErrAccountNotFound
+	}
+
+	pay.Status = types.PaymentStatusFail
+	acc.Balance += pay.Amount
+
+	return nil
+}
+
+
+// Repeat повторяет платёж по идентификатору 
+func (s *Service) Repeat(paymentID string) (*types.Payment, error) {
+	pay, err := s.FindPaymentByID(paymentID)
+	if err != nil {
+	  return nil, err
+	}
+  
+	payment, err := s.Pay(pay.AccountID, pay.Amount, pay.Category)
+	if err != nil {
+	  return nil, err
+	}
+  
+	return payment, nil
+}
+
+// FindFavoriteByID ищет платёж по ID в Избранное
 func (s *Service) FindFavoriteByID(favoriteID string) (*types.Favorite, error) {
 	for _, favorite := range s.favorites {
 		if favorite.ID == favoriteID {
@@ -146,55 +190,7 @@ func (s *Service) FindFavoriteByID(favoriteID string) (*types.Favorite, error) {
 	return nil, ErrFavoriteNotFound
 }
 
-//Deposit method
-func (s *Service) Deposit(accountID int64, amount types.Money) error {
-	if amount < 0 {
-		return ErrAmountMustBePositive
-	}
-
-	account, err := s.FindAccountByID(accountID)
-	if err != nil {
-		return err
-	}
-
-	account.Balance += amount
-	return nil
-}
-
-// Reject метод для отмены покупок
-func (s *Service) Reject(paymentID string) error {
-	pay, err := s.FindPaymentByID(paymentID)
-	if err != nil {
-		return err
-	}
-
-	acc, err := s.FindAccountByID(pay.AccountID)
-	if err != nil {
-		return err
-	}
-
-	pay.Status = types.PaymentStatusFail
-	acc.Balance += pay.Amount
-
-	return nil
-}
-
-// Repeat позволāет по идентификатору повторитþ платёж
-func (s *Service) Repeat(paymentID string) (*types.Payment, error) {
-	pay, err := s.FindPaymentByID(paymentID)
-	if err != nil {
-		return nil, err
-	}
-
-	payment, err := s.Pay(pay.AccountID, pay.Amount, pay.Category)
-	if err != nil {
-		return nil, err
-	}
-
-	return payment, nil
-}
-
-// FavoritePayment добавления новых Избранных
+// FavoritePayment добавляет новый платеж в Избранное
 func (s *Service) FavoritePayment(paymentID string, name string) (*types.Favorite, error) {
 	payment, err := s.FindPaymentByID(paymentID)
 
@@ -215,7 +211,7 @@ func (s *Service) FavoritePayment(paymentID string, name string) (*types.Favorit
 	return newFavorite, nil
 }
 
-//PayFromFavorite для совершения платежа в Избранное
+// PayFromFavorite позволяет совершить платеж из Избранное
 func (s *Service) PayFromFavorite(favoriteID string) (*types.Payment, error) {
 	favorite, err := s.FindFavoriteByID(favoriteID)
 	if err != nil {
@@ -230,92 +226,38 @@ func (s *Service) PayFromFavorite(favoriteID string) (*types.Payment, error) {
 	return payment, nil
 }
 
-// ExportToFile экспартирует данные аккаунтов в файл
+// ExportToFile экспортирует все аккаунты в файл, путь к которому указан в переменной path
 func (s *Service) ExportToFile(path string) error {
 	file, err := os.Create(path)
+
 	if err != nil {
 		log.Print(err)
 		return ErrFileNotFound
 	}
+
 	defer func() {
-		if cerr := file.Close(); cerr != nil {
-			log.Print(cerr)
+		err := file.Close(); 
+		if err != nil {
+			log.Print(err)
 		}
 	}()
 
-	str := ""
+	stringAccount := ""
 
 	for _, acc := range s.accounts {
-		ID := strconv.Itoa(int(acc.ID)) + ";"
-		phone := string(acc.Phone) + ";"
+		ID := strconv.Itoa(int(acc.ID))
+		phone := string(acc.Phone)
 		balance := strconv.Itoa(int(acc.Balance))
 
-		str += ID
-		str += phone
-		str += balance + "|"
+		stringAccount += ID + ";"
+		stringAccount += phone + ";"
+		stringAccount += balance + "|"
 	}
-	_, err = file.Write([]byte(str))
+
+	_, err = file.Write([]byte(stringAccount))
 	if err != nil {
 		log.Print(err)
 		return ErrFileNotFound
-	}
-
-	return nil
-}
-
-// ImportFromFile импортирует данные с аккаунта
-func (s *Service) ImportFromFile(path string) error {
-	file, err := os.Open(path)
-	if err != nil {
-		log.Print(err)
-		return ErrFileNotFound
-	}
-	defer func() {
-		err := file.Close()
-		if err != nil {
-			log.Print(err)
-		}
-	}()
-
-	content := make([]byte, 0)
-	buf := make([]byte, 4)
-	for {
-		read, err := file.Read(buf)
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Print(err)
-			return ErrFileNotFound
-		}
-		content = append(content, buf[:read]...)
-	}
-	data := string(content)
-
-	accounts := strings.Split(string(data), "|")
-	if len(accounts) > 0 {
-		accounts = accounts[:len(accounts)-1] // это фигня служит для того чтоб цикл пощел на n - 1, т.к split добавил в слайс ещё один элемент (причём пустой)
-	}
-	for _, acc := range accounts {
-		splits := strings.Split(acc, ";")
-
-		id, err := strconv.Atoi(splits[0])
-		if err != nil {
-			return err
-		}
-
-		balance, err := strconv.Atoi(splits[2])
-		if err != nil {
-			return err
-		}
-
-		newAccount := &types.Account{
-			ID:      int64(id),
-			Phone:   types.Phone(splits[1]),
-			Balance: types.Money(balance),
-		}
-
-		s.accounts = append(s.accounts, newAccount)
 	}
 
 	return nil
@@ -342,7 +284,7 @@ func (s *Service) Export(dir string) error {
 		file.WriteString(str)
 	}
 	if len(s.payments) > 0 {
-		file, err := os.OpenFile(dir+"/payments.dump", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0666)
+		file, _ := os.OpenFile(dir+"/payments.dump", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0666)
 
 		defer func() {
 			if cerr := file.Close(); cerr != nil {
@@ -361,7 +303,7 @@ func (s *Service) Export(dir string) error {
 	}
 
 	if len(s.favorites) > 0 {
-		file, err := os.OpenFile(dir+"/favorites.dump", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0666)
+		file, _ := os.OpenFile(dir+"/favorites.dump", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0666)
 
 		defer func() {
 			if cerr := file.Close(); cerr != nil {
@@ -377,6 +319,72 @@ func (s *Service) Export(dir string) error {
 			str += fmt.Sprint(v.ID) + ";" + fmt.Sprint(v.AccountID) + ";" + fmt.Sprint(v.Amount) + ";" + fmt.Sprint(v.Category) + "\n"
 		}
 		file.WriteString(str)
+	}
+
+	return nil
+}
+
+// ImportFromFile импортирует все данные из файла, путь к которому указан в переменной path
+func (s *Service) ImportFromFile(path string) error {
+	file, err := os.Open(path)
+
+	if err != nil {
+		log.Print(err)
+		return ErrFileNotFound
+	}
+
+	defer func() {
+		err := file.Close(); 
+		if err != nil {
+			log.Print(err)
+		}
+	}()
+
+	content := make([]byte, 0)
+	buf := make([]byte, 4)
+	for {
+		read, err := file.Read(buf)
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Print(err)
+			return ErrFileNotFound
+		}
+		content = append(content, buf[:read]...)
+	}
+
+	data := string(content)
+
+	accountsString := strings.Split(string(data), "|")
+
+	if len(accountsString) > 0 {
+		accountsString = accountsString[:len(accountsString)-1]
+	}
+
+	for _, acc := range accountsString {
+		fields := strings.Split(acc, ";")
+
+		ID, err := strconv.Atoi(fields[0])
+		if err != nil {
+			return err
+		}
+
+		phone := fields[1]
+
+		balance, err := strconv.Atoi(fields[2])
+		if err != nil {
+			return err
+		}
+
+		account := &types.Account{
+			ID:      int64(ID),
+			Phone:   types.Phone(phone),
+			Balance: types.Money(balance),
+		}
+
+		s.accounts = append(s.accounts, account)
 	}
 
 	return nil
@@ -412,6 +420,8 @@ func (s *Service) Import(dir string) error {
 			flag := true
 			for _, v := range s.accounts {
 				if v.ID == id {
+					v.Phone = types.Phone(strArrAcount[1])
+					v.Balance = types.Money(balance)
 					flag = false
 				}
 			}
@@ -457,6 +467,10 @@ func (s *Service) Import(dir string) error {
 			flag := true
 			for _, v := range s.payments {
 				if v.ID == id {
+					v.AccountID = aid
+					v.Amount = types.Money(amount)
+					v.Category = types.PaymentCategory(strArrAcount[3])
+					v.Status = types.PaymentStatus(strArrAcount[4])
 					flag = false
 				}
 			}
@@ -504,6 +518,9 @@ func (s *Service) Import(dir string) error {
 			flag := true
 			for _, v := range s.favorites {
 				if v.ID == id {
+					v.AccountID = aid
+					v.Amount = types.Money(amount)
+					v.Category = types.PaymentCategory(strArrAcount[3])
 					flag = false
 				}
 			}
@@ -522,23 +539,24 @@ func (s *Service) Import(dir string) error {
 	return nil
 }
 
-// ExportAccountHistory ...
+//ExportAccountHistory вытаскивает все платежи конкретного аккаунта, если их нет - возвращает ошибку
 func (s *Service) ExportAccountHistory(accountID int64) ([]types.Payment, error) {
-	acc, err := s.FindAccountByID(accountID)
+
+	account, err := s.FindAccountByID(accountID)
+
 	if err != nil {
 		return nil, err
 	}
 
 	var payments []types.Payment
-
-	for _, pay := range s.payments {
-		if pay.AccountID == acc.ID {
+	for _, v := range s.payments {
+		if v.AccountID == account.ID {
 			data := types.Payment{
-				ID:        pay.ID,
-				AccountID: pay.AccountID,
-				Amount:    pay.Amount,
-				Category:  pay.Category,
-				Status:    pay.Status,
+				ID:        v.ID,
+				AccountID: v.AccountID,
+				Amount:    v.Amount,
+				Category:  v.Category,
+				Status:    v.Status,
 			}
 			payments = append(payments, data)
 		}
@@ -546,42 +564,47 @@ func (s *Service) ExportAccountHistory(accountID int64) ([]types.Payment, error)
 	return payments, nil
 }
 
-//HistoryToFiles ...
+//HistoryToFiles сохраняет данные из предыдущего метода
 func (s *Service) HistoryToFiles(payments []types.Payment, dir string, records int) error {
 
-	str := ""
+	if len(payments) > 0 {
+		if len(payments) <= records {
+			file, _ := os.OpenFile(dir+"/payments.dump", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0666)
+			defer file.Close()
 
-	if len(payments) > 0 && len(payments) <= records {
-		file, _ := os.OpenFile(dir+"/payments.dump", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0666)
-		defer file.Close()
+			var str string
+			for _, v := range payments {
+				str += fmt.Sprint(v.ID) + ";" + fmt.Sprint(v.AccountID) + ";" + fmt.Sprint(v.Amount) + ";" + fmt.Sprint(v.Category) + ";" + fmt.Sprint(v.Status) + "\n"
+			}
+			file.WriteString(str)
+		} else {
 
-		for _, v := range payments {
-			str += fmt.Sprint(v.ID) + ";" + fmt.Sprint(v.AccountID) + ";" + fmt.Sprint(v.Amount) + ";" + fmt.Sprint(v.Category) + ";" + fmt.Sprint(v.Status) + "\n"
-		}
-		file.WriteString(str)
-	} else {
-		k := 0 // limit on record
-		t := 1 // count for files
-		var file *os.File
-		for _, v := range payments {
-			if k == 0 {
+			var str string
+			k := 0
+			t := 1
+			var file *os.File
+			for _, v := range payments {
+				if k == 0{
 				file, _ = os.OpenFile(dir+"/payments"+fmt.Sprint(t)+".dump", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0666)
+				}
+				k ++
+				str = fmt.Sprint(v.ID) + ";" + fmt.Sprint(v.AccountID) + ";" + fmt.Sprint(v.Amount) + ";" + fmt.Sprint(v.Category) + ";" + fmt.Sprint(v.Status) + "\n"
+				_, err = file.WriteString(str)
+				if k == records{
+					str = ""
+					t ++
+					k = 0;
+					file.Close()
+				}
 			}
-			k++
-			str = fmt.Sprint(v.ID) + ";" + fmt.Sprint(v.AccountID) + ";" + fmt.Sprint(v.Amount) + ";" + fmt.Sprint(v.Category) + ";" + fmt.Sprint(v.Status) + "\n"
-			_, err = file.WriteString(str)
-			if k == records { // если лимит был дастигнут, то обнулить "записи"
-				str = ""
-				t++
-				k = 0
-				file.Close()
-			}
+
 		}
 	}
+
 	return nil
 }
 
-//SumPayments ...
+//SumPayments суммирует платежи
 func (s *Service) SumPayments(goroutines int) types.Money {
 	wg := sync.WaitGroup{}
 	mu := sync.Mutex{}
@@ -625,7 +648,7 @@ func (s *Service) SumPayments(goroutines int) types.Money {
 	return types.Money(sum)
 }
 
-//FilterPayments ...
+//FilterPayments отфильтровывает платежи, выдавая нам только те, у которых accountID равен переданному
 func (s *Service) FilterPayments(accountID int64, goroutines int) ([]types.Payment, error) {
 
 	account, err := s.FindAccountByID(accountID)
@@ -638,7 +661,7 @@ func (s *Service) FilterPayments(accountID int64, goroutines int) ([]types.Payme
 	mu := sync.Mutex{}
 	kol := 0
 	i := 0
-	ps := []types.Payment{}
+	var ps []types.Payment
 	if goroutines == 0 {
 		kol = len(s.payments)
 	} else {
@@ -662,7 +685,7 @@ func (s *Service) FilterPayments(accountID int64, goroutines int) ([]types.Payme
 				}
 			}
 			mu.Lock()
-			ps = pays[:]
+			ps = append(ps, pays...)
 			mu.Unlock()
 
 		}(i)
@@ -684,19 +707,19 @@ func (s *Service) FilterPayments(accountID int64, goroutines int) ([]types.Payme
 			}
 		}
 		mu.Lock()
-		ps = pays
+		ps = append(ps, pays...)
 		mu.Unlock()
 
 	}()
 	wg.Wait()
-	if len(ps) == 0 {
+	if len(ps)==0{
 		return nil, nil
 	}
-	return ps, nil
+	return  ps, nil
 }
 
-//FilterPaymentsByFn ...
-func (s *Service) FilterPaymentsByFn(filter func(payment types.Payment) bool, goroutines int) ([]types.Payment, error) {
+//FilterPaymentsByFn отфильтровывает платежи, выдавая только те где filter(payment) == true
+func (s *Service) FilterPaymentsByFn(filter func(payment types.Payment) bool, goroutines int,) ([]types.Payment, error){
 
 	wg := sync.WaitGroup{}
 	mu := sync.Mutex{}
@@ -728,7 +751,7 @@ func (s *Service) FilterPaymentsByFn(filter func(payment types.Payment) bool, go
 				}
 			}
 			mu.Lock()
-			ps = pays[:]
+			ps = append(ps, pays...)
 			mu.Unlock()
 
 		}(i)
@@ -753,46 +776,42 @@ func (s *Service) FilterPaymentsByFn(filter func(payment types.Payment) bool, go
 			}
 		}
 		mu.Lock()
-		ps = pays[:]
+		ps = append(ps, pays...)
 		mu.Unlock()
 
 	}()
 	wg.Wait()
-	if len(ps) == 0 {
+	if len(ps) == 0{
 		return nil, nil
 	}
-	return ps, nil
+	return  ps, nil
 }
-
-// SumPaymentsWithProgress суммируем и делим все платежи по кусочком
+//SumPaymentsWithProgress делит платежи на куски по 100_000 платежей в каждом и суммирует их параллельно друг другу
 func (s *Service) SumPaymentsWithProgress() <-chan types.Progress {
-	size := 100_0000
-
-	amountOfMoney := []types.Money{}
-	for _, pay := range s.payments {
-		amountOfMoney = append(amountOfMoney, pay.Amount)
-	}
+	sizeOfUnit := 100_0000 		/* когда условие и требование в задаче не совпадают :) */
 
 	wg := sync.WaitGroup{}
-	goroutines := (len(amountOfMoney) + 1) / size
-	if goroutines <= 0 {
-		goroutines = 1
+	goroutines := len(s.payments) / sizeOfUnit /* определяем количество горутин - сколько кусков потребуется сложить*/
+	if goroutines <= 1 {
+		goroutines = 1	
+	/* на случай если платеж всего один (или их нет) */
 	}
 	ch := make(chan types.Progress)
+
 	for i := 0; i < goroutines; i++ {
 		wg.Add(1)
-		go func(ch chan<- types.Progress, amountOfMoney []types.Money) {
-			sum := 0
+		go func(ch chan <- types.Progress, payments []*types.Payment) {
+			//defer close(ch)
+			var sum types.Money = 0
 			defer wg.Done()
-			for _, val := range amountOfMoney {
-				sum += int(val)
-
+			for _, pay := range payments {
+				sum += pay.Amount
 			}
 			ch <- types.Progress{
-				Part:   len(amountOfMoney),
-				Result: types.Money(sum),
+				Part:   len(payments), 
+				Result: sum,
 			}
-		}(ch, amountOfMoney)
+		}(ch, s.payments)
 	}
 
 	go func() {
@@ -801,4 +820,4 @@ func (s *Service) SumPaymentsWithProgress() <-chan types.Progress {
 	}()
 
 	return ch
-}
+} 
